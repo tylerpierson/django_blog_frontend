6b4bd76e7ebdcd2f72eaa1e612e88b35e9ee2315
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-07-12 13:02:29 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Simple React Page</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
